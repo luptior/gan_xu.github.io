3f757eecde8fbb30fa5619a0602f6b2bc55c84cc
Gan Xu <gan.xu@wustl.edu>
2020-10-25 04:38:32 -0500

    add header and footer

---
+++ src/footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <info>Languages:</info>
+                <br/>
+                <info>Python, Java, Go, Shell, JavaScript, HTML, CSS, C/C++</info>
+                <br/>
+                <br/>
+                <info>Tools:</info>
+                <br/>
+                <info>Git, Maven, Gradle, Kafka, Flask, Docker, Spring Boot/MVC, Node.js, MySQL, MongoDB, React,
+                    GraphQL
+                </info>
+                <br/>
+                <br/>
+                <info><a href="courses.html">Related Courses</a></info>
+                <br/>
+                <br/>
+                <a href="mailto:gan.xu@wustl.edu"><img src="http://simpleicons.org/icons/mail-dot-ru.svg" width="35px"
+                                                       height="35px"  alt="mail_svg"/></a>
+                <a href="https://github.com/luptior"><img src="https://simpleicons.org/icons/github.svg" width="35px"
+                                                          height="35px"  alt="github_svg"/></a>
+                <a href="https://www.instagram.com/aspartame_xu/"><img src="http://simpleicons.org/icons/instagram.svg"
+                                                                       width="35px" height="35px"
+                                                                       alt="ins_svg"/></a>
+                <a href="https://www.linkedin.com/in/ganxu/"><img src="http://simpleicons.org/icons/linkedin.svg"
+                                                                  width="35px" height="35px"
+                                                                  alt="linkedin_svg"/></a>
+
+                <br/>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <header>
+                <img className="img-circle" src="./resources/icon.jpg" style="width:150px;height:150px;"
+                     title="Portrait" alt="Portrait"/>
+                    <br/>
+                    <h2>徐甘 Gan Xu</h2>
+
+                    <br/>
+                    <info>Master of Science @</info>
+                    <br/>
+                    <info>Washington University in St. Louis, 2021</info>
+                    <br/>
+                    <br/>
+                    <info>Previous:</info>
+                    <br/>
+                    <info>Assistant Bioinformatic Analyst@</info>
+                    <br/>
+                    <info>Jones' Lab, UNC Chapel Hill</info>
+                    <br/>
+                    <br/>
+                    <info>Bachelor of Science @</info>
+                    <br/>
+                    <info>UNC Chapel Hill, 2017</info>
+
+            </header>
+        </div>
+    );
+}
+export default Footer;
